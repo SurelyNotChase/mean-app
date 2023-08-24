8ee9245597ffac77ff9703d2673111cec6f40ec9
SurelyNotChase <chase.ler@outlook.com>
2023-08-24 11:35:03 -0400

    first edits

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css'],
+})
+export class PostListComponent implements OnInit {
+
+  posts = [];
+  private postsSub: Subscription;
+
+  //'public' creates property, must be added to providers OR decorate service with Injectable from Angular Core
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit(): void {
+
+    this.posts = this.postsService.getPosts();
+    this.postsService.getPostUpdateListener()
+      .subscribe(
+        (posts:Post[]) =>{
+        this.posts=posts;
+      }
+      // ,
+      // () =>{
+      //   //err
+      // },
+      // () =>{
+      //   //finished
+      // },
+    );
+
+  }
+
+  ngOnDestroy(){
+    this.postsSub.unsubscribe(); //prevents memory leaks
+  }
+}
